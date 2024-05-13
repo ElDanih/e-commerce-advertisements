7c45d84887ac2806e971686e926494d51c71f7ef
eldanih_ <dandrescalderon@gmail.com>
2024-05-13 18:46:36 -0500

    Solucion algoritmo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        solution();
     }
 
+    private static void solution() {
+        System.out.println(changeAds(100));
+    }
+
+    public static int changeAds(int base10) {
+        String binary = Integer.toString(base10, 2);
+        StringBuilder sb = new StringBuilder();
+        char[] chars = binary.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            var r = chars[i] == '0' ? '1' : '0';
+            sb.append(r);
+        }
+        return Integer.parseInt(sb.toString(), 2);
+    }
 
 }
